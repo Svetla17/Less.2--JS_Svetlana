84b0828a2b61b688b9a6cc4b2fe44c7f0dd45203
Svetla17 <87719525+Svetla17@users.noreply.github.com>
2021-11-09 21:59:41 +0300

    Add files via upload
    09.11/2021

---
+++ modules/module1.js
@@ -0,0 +1,29 @@
+// export const sum = (a, b) => a + b;
+
+// const sumArray = (arr) => arr.reduce((accum, val) => accum + val, 0);
+
+// export const VERSION = "3";
+
+// export default {
+//     name: "name",
+//     functionality: "sum",
+// };
+
+
+// export class Modal {
+//     create() {
+//         let div = document.createElement("div");
+//     }
+// }
+
+// let div = document.createElement("div");
+// div.append.body;
+// div.classList.add("modal")
+// modal.style.display = "block"
+// modal.style.width = '100px';
+// modal.style.height = '100px';
+
+
+
+
+
---
+++ script0911.js
@@ -0,0 +1,18 @@
+
+// import { sum } from "./modules/module1.js";
+
+// import { sum, sumArray, VERSION } from "./modules/module1.js";
+
+// console.log(sum(5, 6));
+
+// console.log(sumArray([5, 1, 3, 6]));
+
+// console.log(VERSION);
+
+// import module1, { sum } from "./modules/module1.js";
+
+// console.log(module1);
+
+// import { sum as sumInScript } from "./modules/module2.js";
+// console.log(sumInScript(5, 7));
+
